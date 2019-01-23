a14c8bfbee642424a852e853aa2594b09ab43656
Akio Ikeda <principewar@live.com>
2019-01-23 10:33:43 -0500

    Initial commit
    NoSleep app prevents Windows from the sleep.
    It simulate key down event (F15) every 59 seconds.
    It is a console app.

---
+++ NoSleep/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace NoSleep
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            InitTimer();
+            Console.WriteLine("NoSleep started");
+
+            // Doesn't close the console with F15
+            while (Console.ReadKey(true).Key == ConsoleKey.F15)
+            {
+                Console.Read();
+            }
+        }
+
+        private static void InitTimer()
+        {
+            /*
+             *  Perform KeyPress method every interval
+             */
+            int interval = 59000;
+            Timer timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(KeyPress);
+            timer.Interval = interval;
+            timer.Enabled = true;
+            //timer.AutoReset = true;
+        }
+
+        private static void KeyPress(Object source, ElapsedEventArgs e)
+        {
+            /*  
+             *  Simulate key down (F15) using InputSimulator library from 
+             *  NuGet packages
+             */
+            InputSimulator sim = new InputSimulator();
+            sim.Keyboard.KeyPress(VirtualKeyCode.F15);
+        }
+    }
+}
