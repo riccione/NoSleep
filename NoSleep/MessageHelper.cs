cda03c0f406f48bb4db4c0d96157572f6c9724af
Akio Ikeda <principewar@live.com>
2019-01-30 11:25:23 -0500

    Add CPU usage stats and split methods in MessageHelper

--- NoSleep/MessageHelper.cs
+++ NoSleep/MessageHelper.cs
@@ -12,13 +12,24 @@ namespace NoSleep
 
         public string GetMessage()
         {
+            Performance p = new Performance;
+            string cpu = Convert.ToString(p.GetCpuUsage());
+            string time = CountUsageTime();
+            string output = Program.prefix
+                + time
+                + " | " 
+                + cpu;
+            return output;
+        }
+
+        private string CountUsageTime()
+        {
             int milliSecs = IterateCounter() * Program.interval;
             int hours = milliSecs / 3600000;
             int mins = (milliSecs % 3600000) / 60000;
             // Make sure you use the appropriate decimal separator
             string time = string.Format("{0:D2}:{1:D2}:{2:D2}.{3:D3}", hours, mins, milliSecs % 60000 / 1000, milliSecs % 1000);
-            string output = Program.prefix + time;
-            return output;
+            return time;
         }
 
         private int IterateCounter()
---
+++ NoSleep/Performance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSleep
+{
+    class Performance
+    {
+        public int GetCpuUsage()
+        {
+            // https://gavindraper.com/2011/03/01/retrieving-accurate-cpu-usage-in-c/
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", "MyComputer");
+            cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            return (int)cpuCounter.NextValue();
+        }
+    }
+}
